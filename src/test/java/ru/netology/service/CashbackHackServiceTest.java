ee476017f5cd7837c2746e4eae6e3fe0c803ef5b
Dmitrii Odynets <dimosaintp@gmail.com>
2024-07-08 12:50:36 +0300

    Tests written on Junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,24 @@
 package ru.netology.service;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldAdviceIfAmountNull() {
+        Assert.assertEquals(service.remain(0), 1_000);
+    }
+
+    @Test
+    public void shouldAdviceIfAmountLessThanBoundary() {
+        Assert.assertEquals(service.remain(800), 200);
+
+    }
+
+    @Test
+    public void shouldAdviceIfAmountMoreThanBoundary() {
+        Assert.assertEquals(service.remain(1200), 0);
+    }
 }
