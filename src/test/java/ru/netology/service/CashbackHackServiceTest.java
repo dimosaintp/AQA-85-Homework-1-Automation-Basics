cc6b31daa24181d3f59dc717fd5d55a005f04d07
Dmitrii Odynets <dimosaintp@gmail.com>
2024-07-08 12:40:36 +0300

    Tests written on Testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,25 @@
 package ru.netology.service;
 
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldAdviceIfAmountNull () {
+        Assert.assertEquals(service.remain(0), 1_000);
+    }
+
+    @Test
+    public void shouldAdviceIfAmountLessThanBoundary () {
+        Assert.assertEquals(service.remain(800), 200);
+
+    }
+
+    @Test
+    public void shouldAdviceIfAmountMoreThanBoundary () {
+        Assert.assertEquals(service.remain(1200), 0);
+    }
 }
